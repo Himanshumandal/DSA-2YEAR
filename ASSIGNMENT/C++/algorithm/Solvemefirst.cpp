90e3e4055d0ed93c8fa519e79c14bea5cc5f05c3
Himanshumandal <113971156+Himanshumandal@users.noreply.github.com>
2022-10-07 12:16:00 +0530

    Create Solvemefirst.cpp

---
+++ ASSIGNMENT/C++/algorithm/Solvemefirst.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    int n;
+    n=a+b;
+    return n;
+  
+}
+
+int main() {
+    int num1, num2;
+    int sum;
+    cin>>num1>>num2;
+    sum = solveMeFirst(num1,num2);
+    cout<<sum;
+    return 0;
+}
