6d5f27c54a9ef37e9d78787c600edbf9531d1551
Himanshu Mandal <113971156+Himanshumandal@users.noreply.github.com>
2023-01-15 01:06:04 +0530

    Create find-all-duplicates-in-an-array.cpp

---
+++ ASSIGNMENT/C++/Hashmap/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_map<int,int>m;
+        vector<int>n;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto it:m){
+            if(it.second==2){
+
+                n.push_back(it.first);
+            }
+        }
+        return n;
+    }
+};
